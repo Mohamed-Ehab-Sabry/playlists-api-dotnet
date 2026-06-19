dbf1ace6f6ac0c09609806fa46f01561b70643e1
Mohamed Ehab Sabry <messk8@gmail.com>
2026-06-19 17:48:14 +0300

    feat: create Playlist, Song, and PlaylistSong entities classes

---
+++ PlaylistService/PlaylistService.Domain/Entities/Playlist.cs
@@ -0,0 +1,31 @@
+
+using System.Data;
+
+namespace PlaylistService.Domain.Entities;
+
+public class Playlist
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+
+    private readonly List<PlaylistSong> _playlistSongs = new();
+    public IReadOnlyCollection<PlaylistSong> PlaylistSongs => _playlistSongs.AsReadOnly();
+
+    public Playlist(string name, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        UserId = userId;
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+
+    public void SoftDelete()
+    {
+        IsDeleted = true;
+    }
+
+}
