9eb90600fb964ceb5ef2ccb4b110bdaa1ddd057d
Mohamed Ehab Sabry <messk8@gmail.com>
2026-06-21 11:00:17 +0300

    feat: implement integration tests for PlaylistService endpoints

--- PlaylistService/PlaylistService.Api/Program.cs
+++ PlaylistService/PlaylistService.Api/Program.cs
@@ -39,3 +39,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+public partial class Program { }
---
+++ PlaylistService/PlaylistService.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PlaylistService.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+}
